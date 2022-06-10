80645e66ecefa617332a486ec0d9e7c120f28a8b
Di Wu <dwu233@gmail.com>
2022-06-10 18:08:41 -0400

    set up database and created the first three users(us). Yay

---
+++ backend/db/seeds/01_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name,email,password) VALUES ('Eugene','e@e','password');
+INSERT INTO users (name,email,password) VALUES ('Heron','h@h','password');
+INSERT INTO users (name,email,password) VALUES ('Di','d@d','password');
