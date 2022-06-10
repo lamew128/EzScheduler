ef49d00eae9f422a60e464aaa11cdff836bd51fc
Di <dwu233@gmail.com>
2022-06-10 22:33:15 +0000

    testing the routes from backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+import {getAllUsers} from "../db/db.js"
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/test', function(req, res, next) {
+  getAllUsers()
+    .then ((data) => {
+      console.log("dataaaaaaa" + res.json(data))
+    });
+  }
+);
 
 module.exports = router;
