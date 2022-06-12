b54bfa1e3facb9c97552d3f5fde428e2b6972e5a
Di Wu <dwu233@gmail.com>
2022-06-12 14:22:25 -0400

    change directories for deploymenbt

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const db = require("./db/db.js");
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const eventRouter = require('./routes/event');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter(db));
+app.use('/event', eventRouter(db));
+
+module.exports = app;
