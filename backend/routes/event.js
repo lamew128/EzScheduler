e2375eb12a635fbbcb07b14eafed11dc04e0705d
Eugene <lamew128@gmail.com>
2022-06-12 14:30:06 -0400

    add get event routers with user id

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -7,17 +7,32 @@ module.exports = (db) => {
 
   /* GET event listing. */
   router.get('/', (req, res) => {
-    db.getEvents()
+    db.getAllEvents()
       .then((data) => {
         console.log("dataaaaaaa" + data);
         res.json(data);
       });
-    }
-  );
+  });
 
-  //router.get
+  //all event created by user with given id
+  router.get('/created/:user', (req, res) => {
+    const userId = req.params.user;
+    db.myCreatedEvents(userId)
+      .then((data) => {
+        console.log("dataaaaaaa" + data);
+        res.json(data);
+      })
+  });
 
-  
+  //all event related to the user with given id
+  router.get('/all/:user', (req, res) => {
+    const userId = req.params.user;
+    db.getUpcomingEvents(userId)
+      .then((data) => {
+        console.log("dataaaaaaa" + data);
+        res.json(data);
+      })
+  });
 
   return router;
 };
