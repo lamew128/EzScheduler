09754534de3b8360744bf424cf75beb4cf201856
lamew128 <lamew128@gmail.com>
2022-06-12 14:10:23 -0400

    .

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -6,7 +6,7 @@ router.use(bodyParser.urlencoded({ extended: false }));
 module.exports = (db) => {
 
   /* GET event listing. */
-  router.get('/test', (req, res) => {
+  router.get('/', (req, res) => {
     db.getEvents()
       .then((data) => {
         console.log("dataaaaaaa" + data);
@@ -15,24 +15,7 @@ module.exports = (db) => {
     }
   );
 
-  /**
-   * Check if a user exists with a given username and password
-   * @param {String} email
-   * @param {String} password
-   */
-  const login = function(email, password) {
-    return db.getUserWithEmail(email)
-      .then((email) => {
-        if (!email) {
-          return;//no email
-        }
-        if (password === email.password) {
-          return email;//wrong passwod
-        }
-        return null;
-      });
-  };
-  exports.login = login;
+  //router.get
 
   
 
