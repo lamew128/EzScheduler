7ebab6dad05d88640b02245f7321c578faad1b06
Heron Feijao <heronfeijao@gmail.com>
2022-06-12 16:32:47 -0300

    merging login-register into main

--- backend/db/db.js
+++ backend/db/db.js
@@ -22,7 +22,7 @@ const pool = new Pool(dbParams);
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
- const addUser =  function(user) {
+const addUser = function(user) {
   return pool
     .query(
       `INSERT INTO users (name, email, password)
@@ -56,102 +56,102 @@ const getUserWithEmail = function(email) {
 
 const getAllUsers = () => {
   return pool
-  .query(
-    `SELECT * FROM users;`)
-  .then((data) => {
-    const users = data.rows;
-    return users;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .query(
+      `SELECT * FROM users;`)
+    .then((data) => {
+      const users = data.rows;
+      return users;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 const getAllEvents = () => {
   return pool
-  .query(
-    `SELECT * FROM events;`)
-  .then((data) => {
-    const events = data.rows;
-    return events;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .query(
+      `SELECT * FROM events;`)
+    .then((data) => {
+      const events = data.rows;
+      return events;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 //get the events that created by the given user
 //output: [event_id, title, description, start_time, end_time, lat, long]
 const myCreatedEvents = (user) => {
   return pool
-  .query(
-    `
+    .query(
+      `
     SELECT events.id as event_id, events.name as title, events.description, events.start_time, events.end_time, events.latitude as lat, events.longtitude as long FROM events
     WHERE user_id = $1;
     `, [user])
-  .then((data) => {
-    const events = data.rows;
-    return events;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((data) => {
+      const events = data.rows;
+      return events;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 //all event belongs to user or got invited
 //output: [event_id, creator, title, description, start_time, end_time, lat, long]
 const getUpcomingEvents = (user) => {
   return pool
-  .query(
-    `
+    .query(
+      `
     SELECT distinct events.id as event_id, events.user_id as creator, events.name as title, events.description, events.start_time, events.end_time, events.latitude as lat, events.longtitude as long
     From EVENTS
     Join event_invitees ON events.id = event_invitees.event_id
     WHERE event_invitees.user_id = $1;
     `, [user])
-  .then((data) => {
-    const events = data.rows;
-    return events;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((data) => {
+      const events = data.rows;
+      return events;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 //create an event
 const createEvent = (event) => {
   let eventParams = [event.title, event.description, event.startTime, event.endTime, event.lat, event.long, event.creator];
   return pool
-  .query(
-    `
+    .query(
+      `
     INSERT INTO events (name, description, start_time, end_time, latitude, longtitude, user_id)
     VALUES($1, $2, $3, $4, $5, $6, $7)
     RETURNING *;
     `, eventParams)
-  .then((data) => {
-   
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((data) => {
+
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 //create an event
 const invite = (invite) => {
   let inviteParams = [invite.eventId, invite.userId, invite.response];
   return pool
-  .query(
-    `
+    .query(
+      `
     INSERT INTO event_invitees (event_id, user_id, response) 
     VALUES ($1, $2, $3)
     RETURNING *;
     `, inviteParams)
-  .then((data) => {
-    
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((data) => {
+
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 //edit event
@@ -159,72 +159,72 @@ const invite = (invite) => {
 const editEvent = (event) => {
   let eventParams = [event.title, event.description, event.startTime, event.endTime, event.lat, event.long, event.creator, event.id];
   return pool
-  .query(
-    `
+    .query(
+      `
     UPDATE events
     SET name = $1, description = $2, start_time = $3, end_time = $4, latitude = $5, longtitude = $6, user_id = $7
     WHERE id = $8
     RETURNING *;
     `, eventParams)
-  .then((data) => {
-    
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((data) => {
+
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 //delete an event with given id
 const deleteEvent = (eventId) => {
   return pool
-  .query(
-    `
+    .query(
+      `
     DELETE FROM events
     WHERE id = $1
     RETURNING *;  
     `, eventId)
-  .then((data) => {
-    
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((data) => {
+
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 //edit inv (change response)
 const responseInvite = (invite) => {
   let inviteParams = [invite.id, invite.response];
   return pool
-  .query(
-    `
+    .query(
+      `
     UPDATE events_invitees
     SET response = $2
     WHERE id = $1
     RETURNING *;
     `, inviteParams)
-  .then((data) => {
-    
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((data) => {
+
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 //delete inv
 const deleteInvite = (id) => {
   return pool
-  .query(
-    `
+    .query(
+      `
     DELETE FROM events_invitees
     WHERE id = $1
     RETURNING *;
     `, id)
-  .then((data) => {
-    
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((data) => {
+
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
@@ -234,10 +234,10 @@ module.exports = {
   getAllEvents,
   myCreatedEvents,
   getUpcomingEvents,
-  createEvent, 
+  createEvent,
   invite,
   editEvent,
   deleteEvent,
   responseInvite,
   deleteInvite
-}
+};
