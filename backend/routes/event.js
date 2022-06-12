723f40a4f21bc61645e45eba19ce502fcec15c73
Eugene <lamew128@gmail.com>
2022-06-12 15:01:27 -0400

    add all event routes version 1

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -9,7 +9,6 @@ module.exports = (db) => {
   router.get('/', (req, res) => {
     db.getAllEvents()
       .then((data) => {
-        console.log("dataaaaaaa" + data);
         res.json(data);
       });
   });
@@ -19,7 +18,6 @@ module.exports = (db) => {
     const userId = req.params.user;
     db.myCreatedEvents(userId)
       .then((data) => {
-        console.log("dataaaaaaa" + data);
         res.json(data);
       })
   });
@@ -28,6 +26,48 @@ module.exports = (db) => {
   router.get('/all/:user', (req, res) => {
     const userId = req.params.user;
     db.getUpcomingEvents(userId)
+      .then((data) => {
+        res.json(data);
+      })
+  });
+
+  //create a new event
+  //event should contain { title, description, startTime, endTime, lat, long, creator }
+  router.post('/new', (req, res) => {
+    const { event } = req.body.event;
+    db.createEvent(event)
+      .then((data) => {
+        console.log("dataaaaaaa" + data);
+        res.json(data);
+      })
+  });
+
+  //edit an event
+  //event should contain { title, description, startTime, endTime, lat, long, creator, id } id = event id
+  router.put('/', (req, res) => {
+    const { event } = req.body.event;
+    db.editEvent(event)
+      .then((data) => {
+        console.log("dataaaaaaa" + data);
+        res.json(data);
+      })
+  });
+
+  //delete an event
+  router.delete('/:id', (req, res) => {
+    const eventId = req.params.id;
+    db.deleteEvent(eventId)
+      .then((data) => {
+        console.log("dataaaaaaa" + data);
+        res.json(data);
+      })
+  });
+
+  //update a response
+  //invite should contain { id, response }
+  router.put('/response', (req, res) => {
+    const { invite } = req.body.invite;
+    db.responseInvite(invite)
       .then((data) => {
         console.log("dataaaaaaa" + data);
         res.json(data);
