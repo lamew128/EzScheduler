32e40468cd1191de514fc311d8db1d2b158e483e
Di Wu <dwu233@gmail.com>
2022-06-14 16:24:08 -0400

    add axios request to get event details

--- frontend/src/pages/EventMainPage.jsx
+++ frontend/src/pages/EventMainPage.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useEffect} from "react";
 import EventPage from "../components/EventPage/EventPage";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+
+
 
 const EventMainPage = (props) => {
-  const { id } = useParams();
+  const {id} = useParams();
+  useEffect(() => {
+    axios.get(`/event/${id}`).then((event) => {
+      console.log(event.data);
+
+    })
+  }, []);
+
   return (
     <>
       <EventPage eventId={id} />
     </>
   );
 };
+
+
+
+  // useEffect(() => {
+  //   if (userId) {
+  //     axios.get(`/event/${eventId}`).then((event) => {
+  //       console.log(event.data);
+  //       setEvents(event.data);
+  //     });
+  //     setShowEvents(true);
+  //     setEventChange(false);
+  //   } else {
+  //     setShowEvents(false);
+  //     setEventChange(false);
+  //   }
+  // }, [userId, props.cookies.user, eventChange]);
+
+
+  
+
+
 
 export default EventMainPage;
